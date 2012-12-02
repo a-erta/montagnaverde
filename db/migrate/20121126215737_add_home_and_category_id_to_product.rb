91b1bd13654018bdd7a7e82a5b05abc76a3c3167
Alessandro <alessandro.erta@gmail.com>
2012-12-02 17:52:14 +0100

    add product_id and change type

--- db/migrate/20121126215737_add_home_and_category_id_to_product.rb
+++ db/migrate/20121126215737_add_home_and_category_id_to_product.rb
@@ -1,5 +1,6 @@
 class AddHomeAndCategoryIdToProduct < ActiveRecord::Migration
   def change
+    add_column :products, :category_id, :integer
     change_column :products, :category_id, :string
   end
 end
